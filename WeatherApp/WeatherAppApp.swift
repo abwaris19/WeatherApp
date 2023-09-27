f57039db947cec72a07331de91771dedece8aa87
abdulwariskhan <hellokhan.ch@gmail.com>
2023-09-27 10:41:29 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Khan on 27.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
