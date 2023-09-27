ddb2145f2df8748d3a560e549876c62adfa8bfff
abdulwariskhan <hellokhan.ch@gmail.com>
2023-09-27 21:57:46 +0200

    Refactoring and Project Organization

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -11,90 +11,32 @@ struct ContentView: View {
     var body: some View {
       
         ZStack{
-         
-            LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+            BackgroundView(topColor: .blue, bottomColor: Color("background"))
+            LinearGradient(gradient: Gradient(colors: [.blue,Color("background")]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
             
             VStack
             {
-                Text("Winterthru,ZH")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "Winterthur,ZH")
                 
               
                 
-                VStack(spacing: 8)
-                {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:180, height:180)
-                    Text("78°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-
-                }
-                HStack{
-                    Spacer()
-                    VStack (spacing: 10)
-                    {
-                        Text("TUE")
-                            .font(.title)
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width:40, height: 40)
-                        Text("42°")
-                            .font(.title)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack (spacing: 10)
-                    {
-                        Text("WED")
-                            .font(.title)
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width:40, height: 40)
-                        Text("22°")
-                            .font(.title)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack (spacing: 10)
-                    {
-                        Text("THR")
-                            .font(.title)
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width:40, height: 40)
-                        Text("32°")
-                            .font(.title)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack (spacing: 10)
-                    {
-                        Text("FRI")
-                            .font(.title)
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width:40, height: 40)
-                        Text("12°")
-                            .font(.title)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
+                MainWeatherView(imageName: "cloud.sun.fill", temperature: 33)
+        
+                HStack (spacing: 30){
                     
+                   WeatherDayView(dayOfWeeks: "FR", imageName: "cloud.sun.fill", temperature: 12)
+                    
+                    WeatherDayView(dayOfWeeks: "SU", imageName: "sun.max.fill", temperature: 23)
+                    
+                    WeatherDayView(dayOfWeeks: "MO", imageName: "wind.snow", temperature: 24)
+                    
+                    WeatherDayView(dayOfWeeks: "TU", imageName: "sunset.fill", temperature: 11)
+                    
+                    WeatherDayView(dayOfWeeks: "Th", imageName: "snow", temperature: 30)
                 }
+                Spacer()
+                    
+                WeatherAppButton(message: "Tapped", buttonName: "Change the Days")
                 Spacer()
             }
          
@@ -104,6 +46,93 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+       
+        
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeeks: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack
+        {
+            Text(dayOfWeeks)
+                .font(.title)
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .frame(width:40, height: 40)
+            Text(String(temperature)+"°")
+                .font(.title)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View{
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    var body: some View{
+      
+        Text(cityName) .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+        
+    }
+}
+
+struct MainWeatherView: View {
+    var imageName: String
+    var temperature: Int
+    var body: some View {
+        VStack(spacing: 8)
+        {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:180, height:180)
+            Text(String(temperature)+"°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom,40)
+    }
+}
+
+struct WeatherAppButton: View{
+    var message: String
+    var buttonName: String
+    var body: some View{
+        
+        Button{
+            print(message)
+        }
+        label:{
+        Text(buttonName)
+                .frame(width:280, height: 50)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .background(Color.white)
+                .cornerRadius(10.0)
+                
+    }
     }
 }
